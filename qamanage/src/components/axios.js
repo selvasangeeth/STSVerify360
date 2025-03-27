fefd8f10a3b83f99f82b271cde6e7b11c2e593ad
Surya Prabhu T <STS941@spanent.com>
2025-03-27 15:48:05 +0530

    updated axios

--- qamanage/src/components/axios.js
+++ qamanage/src/components/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:9000",  
+  baseURL: process.env.REACT_APP_BACKEND_URL, // Use the environment variableEND_URL, // Use the environment variable
   withCredentials: true,  
 });
 
 export default instance;
-  
